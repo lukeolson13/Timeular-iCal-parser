5914fba9ff14fa73f549deb046c941e0c6385bc0
Luke Olson <lukeolson13@gmail.com>
2018-05-03 07:05:01 -0600

    add requests to grab info auto-magically

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 Instructions:
 1. Timeular:
 	- Go to [timeular (app) profile](https://profile.timeular.com/#/login)
-	- On 'Integrations', download the iCal version of your logged hours (by going to the URL that's generated)
+	- On 'Integrations', click the iCal version of your logged hours and copy the URL
 2. Script:
 	- Download or copy/paste timeular_ical_parser.py from this repo
-	- Open in text editor, and edit time difference field below "if '__name__' == '__main__':"
+	- Open in text editor, and edit the input_url, output_folder, and time difference field below "if '__name__' == '__main__':"
+		- Paste the ical URL in the "input_url" field
+		- Add a folder location where you want your generated file to end up, or leave blank to have it generate wherever you run the script from
 		- You'll need to look up the time difference between yourself and UTC. For example, I'm MST, which is (currently) 6 hours behind, hence the "-6"
-	- Run from command line (make sure you're using python 3): 'python tiemular_ical_parser.py <iCal input file> <csv output file to generate>'
-	- Add option start and end dates:
+	- Run from command line (make sure you're using python 3): 'python tiemular_ical_parser.py'
+	- Add optional start and end dates:
 		- Date inputs take time change into account
 		- If one time spans two days, the start date (before midnight) will be used for the date filter
 3. Use in Excel:
--- timeular_ical_parser.py
+++ timeular_ical_parser.py
@@ -7,11 +7,12 @@ __author__ = 'Luke Olson'
 
 from datetime import datetime, timedelta
 import pandas as pd
+import requests
 from sys import argv
 
-def get_dates():
-	start = input('Enter a start date, or skip for all (MM/DD/YYYY): ')
-	end = input('Enter an end date, or skip for all (MM/DD/YYYY): ')
+def get_string_dates():
+	start = input('Enter a start date, or skip for all (MM-DD-YYYY): ')
+	end = input('Enter an end date, or skip for all (MM-DD-YYYY): ')
 	return start, end
 
 def ical_date_parse(ical_date_time, time_diff):
@@ -27,7 +28,7 @@ def ical_date_parse(ical_date_time, time_diff):
 	'''
 	date_time = datetime.strptime(ical_date_time, '%Y%m%dT%H%M%SZ')
 	date_time += timedelta(hours=time_diff)
-	date = date_time.strftime('%m/%d/%Y')
+	date = date_time.strftime('%m-%d-%Y')
 	time = date_time.strftime('%H:%M:%S')
 	return date_time, date, time
 
@@ -35,17 +36,44 @@ def get_date(str_date):
 	'''
 	Get datetime date from string date
 	Inputs:
-		str_date - string date in format 'MM/DD/YYYY'
+		str_date - string date in format 'MM-DD-YYYY'
 	Returns:
 		Datetime date
 	'''
-	return datetime.strptime(str_date, '%m/%d/%Y')
+	return datetime.strptime(str_date, '%m-%d-%Y')
 
-def parse_ical(ical_file, time_diff, start, end):
+def get_dt_dates(start_in, end_in):
+	'''
+	Get datetime version of dates
+	Inputs:
+		start_in - string version of start date
+		end_in - string version of end date
+	Returns:
+		start_out - datetime version of start date
+		end_out - datetime version of end date
+	'''
+	if start_in:
+		start_out = get_date(start_in)
+	else:
+		start_out = None
+
+	if end_in:
+		end_out = get_date(end_out)
+	else:
+		end_out = None
+
+	return start_out, end_out
+
+def get_entries(ical_url):
+	data = requests.get(ical_url)
+	decoded = data.content.decode('utf-8')
+	return decoded.split('\r\n')
+
+def parse_ical(ical_url, time_diff, start, end):
 	'''
 	Create pandas dataframe from parsed ical Timeular entries
 	Inputs:
-		ical_file - location and name of ical file
+		ical_url - URL of ical file containing Timeular entries
 		time_diff - difference in time between your time zone and UTC
 		start - first day to include in output
 		end - last day to include in output
@@ -53,50 +81,65 @@ def parse_ical(ical_file, time_diff, start, end):
 		Pandas dataframe sorted by date and time
 	'''
 	df = pd.DataFrame(columns=['start day', 'start time', 'end time', 'hours', 'summary', 'description'])
-	with open(ical_file, 'r+') as f:
-		skip = False
-		for line in f:
-			if (':' not in line):
-				# Protect against new line oddities, among others
-				continue
 
-			split = line[:-1].split(':')
-			if (split[0] == 'BEGIN') and (split[1] == 'VEVENT'):
-			    dic = {}
-			    skip = False
+	entry_list = get_entries(ical_url)
 
-			if skip:
-				continue
+	skip = False
+	for line in entry_list:
+		if (':' not in line):
+			# Protect against new line oddities, among others
+			continue
 
-			elif split[0] == 'DTSTART':
-			    begin_date_time, begin_date, begin_time = ical_date_parse(split[1], time_diff)
-			    if start:
-				    if get_date(begin_date) < start:
-				    	skip = True
-				    	continue
-			    dic['start day'] = begin_date
-			    dic['start time'] = begin_time
+		split = line.split(':')
+		if (split[0] == 'BEGIN') and (split[1] == 'VEVENT'):
+		    dic = {}
+		    skip = False
 
-			elif split[0] == 'SUMMARY':
-			    dic['summary'] = split[1]
+		if skip:
+			continue
 
-			elif split[0] == 'DESCRIPTION':
-			    dic['description'] = split[1]
+		elif split[0] == 'DTSTART':
+			begin_date_time, begin_date, begin_time = ical_date_parse(split[1], time_diff)
+			if start:
+			    if get_date(begin_date) < start:
+			    	skip = True
+			    	continue
+			dic['start day'] = begin_date
+			dic['start time'] = begin_time
 
-			elif split[0] == 'DTEND':
-			    end_date_time, end_date, end_time = ical_date_parse(split[1], time_diff)
-			    if end:
-			    	if get_date(end_date) > end:
-				    	skip = True
-				    	continue
-			    diff = (end_date_time - begin_date_time).seconds / 60 / 60
-			    dic['end time'] = end_time
-			    dic['hours'] = round(diff, 3)
+		elif split[0] == 'SUMMARY':
+		    dic['summary'] = split[1]
 
-			elif (split[0] == 'END') and (split[1] == 'VEVENT'):
-				df = df.append(dic, ignore_index=True)
+		elif split[0] == 'DESCRIPTION':
+		    dic['description'] = split[1]
+
+		elif split[0] == 'DTEND':
+		    end_date_time, end_date, end_time = ical_date_parse(split[1], time_diff)
+		    if end:
+		    	if get_date(end_date) > end:
+			    	skip = True
+			    	continue
+		    diff = (end_date_time - begin_date_time).seconds / 60 / 60
+		    dic['end time'] = end_time
+		    dic['hours'] = round(diff, 3)
+
+		elif (split[0] == 'END') and (split[1] == 'VEVENT'):
+			df = df.append(dic, ignore_index=True)
 
 	return df.sort_values(['start day', 'start time'])
+
+def get_output_file(output_folder, start_date, end_date):
+	if start_date:
+		start_out = start_date
+	else:
+		start_out = 'Everything'
+
+	if end_date:
+		end_out = end_date
+	else:
+		end_out = datetime.now().strftime("%m-%d-%Y_%H%M")
+
+	return '{}{}_to_{}.csv'.format(output_folder, start_out, end_out)
 
 def export_to_csv(df, output_file):
 	'''
@@ -108,28 +151,30 @@ def export_to_csv(df, output_file):
 	df.to_csv(output_file, index=False)
 	print('Success! Check the file location: {}'.format(output_file))
 
-def main(time_diff=0):
+def main(ical_url, output_folder, time_diff=0):
 	'''
 	Execute above functions
 	Inputs:
+		ical_url - URL of ical file containing Timeular entries
+		output_folder - folder location to output CSV to
 		time_diff - difference in time between your time zone and UTC
 	'''
-	ical_file = argv[1]
-	output_file = argv[2]
+	start, end = get_string_dates()
+	start_date, end_date = get_dt_dates(start, end)
 
-	start_date, end_date = get_dates()
-	if start_date:
-		start_date = get_date(start_date)
-	if end_date:
-		end_date = get_date(end_date)
+	output_file = get_output_file(output_folder, start, end)
 
-	df = parse_ical(ical_file, time_diff, start_date, end_date)
+	df = parse_ical(ical_url, time_diff, start_date, end_date)
 	# can manipulate pandas dataframe here if needed
 	export_to_csv(df, output_file)
 
 if __name__ == '__main__':
+	# Required input:
+	ical_url = 'https://thirdparty.timeular.com/api/ical/480c0f598f55defd6a8b8e0702c8ddbcd1b1757549840297d7a2fc75880d35fe/calendar.ics'
+	output_folder = '../../hours/excel/'
+
 	# Optional input:
 	time_diff = -6
 
 	# Run functions (don't edit)
-	main(time_diff)
+	main(ical_url, output_folder, time_diff)
